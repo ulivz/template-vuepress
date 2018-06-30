d6075575e64c898586106e1816315c9606c4998b
ULIVZ <472590061@qq.com>
2018-06-30 15:11:24 +0800

    refactor: add some comments and enhance built-in code style

--- template/docs/.vuepress/config.js
+++ template/docs/.vuepress/config.js
@@ -2,29 +2,52 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
-  base: "<%= base %>",
   title: "<%= title %>",
   description: "<%= description %>",
+
+  // See: https://vuepress.vuejs.org/config/#base
+  base: "<%= base %>",
+
+  // Extra tags to be injected to the page HTML <head>
+  // See: https://vuepress.vuejs.org/config/#head
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
+
+  // Global stylus file registration
+  // See: https://vuepress.vuejs.org/config/#stylus
   stylus: {
     import: [path.resolve(__dirname, './style/config.styl')]
   },
+
+  // PWA support
+  // See: https://vuepress.vuejs.org/config/#serviceworker
   serviceWorker: true,
+
+  // See: https://vuepress.vuejs.org/default-theme-config/#homepage
   themeConfig: {
     repo: "<%= username %>/<%= name %>",
     editLinks: true,
     docsDir: 'docs',
     editLinkText: 'Edit this page on GitHub',
     lastUpdated: 'Last Updated',
-    sidebar: autoGetSiderbars()
+    sidebar: inferSiderbars()
   }
 }
 
-function autoGetSiderbars () {
+/**
+ * If you want to create a docs that automatically lists all files in all subdirectories,
+ * This method will help you complete this task.
+ *
+ * If you do not prefer this preset, just remove it and configure it according to the
+ * docs: https://vuepress.vuejs.org/default-theme-config/#sidebar
+ *
+ * @returns {Array}
+ */
+function inferSiderbars () {
+  // You will need to update this config when directory was added or removed.
   const sidebars = [
     { title: 'JavaScript', dirname: 'javascript' },
     { title: 'CSS', dirname: 'css' },
