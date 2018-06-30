220dc62f28c942bcfa3beb68f025ebf5815d6ab4
ULIVZ <472590061@qq.com>
2018-06-30 10:58:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# template-vuepress
+
+> Create a VuePres project with [SAO](https://github.com/egoist/sao).
+
+## Usage
+
+Install [SAO](https://github.com/egoist/sao) first.
+
+```bash
+yarn global add sao
+# or
+npm i -g sao
+```
+
+### From npm
+
+```bash
+sao vuepress my-project
+```
+
+### From git
+
+```bash
+sao haolchen/template-vuepress my-project
+```
+
+## License
+
+MIT &copy; [haolchen](github.com/haolchen)
---
+++ template/docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  base: "<%= base %>",
+  dest: "<%= dest %>",
+  title: "<%= title %>",
+  description: "<%= description %>",
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  stylus: {
+    import: [path.resolve(__dirname, './style/config.styl')]
+  },
+  serviceWorker: true,
+  themeConfig: {
+    repo: "<%= website %>",
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+    sidebar: sidebars.map(({ title, dirname }) => {
+      const dirpath = path.resolve(__dirname, '../' + dirname)
+      return {
+        title,
+        collapsable: false,
+        children: fs
+          .readdirSync(dirpath)
+          .filter(item => item.endsWith('.md') && fs.statSync(path.join(dirpath, item)).isFile())
+          .map(item => dirname + '/' + item.replace(/.md$/, ''))
+      }
+    })
+  }
+}
